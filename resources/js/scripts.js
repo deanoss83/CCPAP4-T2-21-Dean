53417dd1baf7e32b0b7133652dcbaf80511653f8
deanoss83 <dean_flanagan@yahoo.com>
2021-08-08 11:55:02 +1000

    embed youtube video pop up not working

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -20,3 +20,16 @@ function openNav() {
   /*this is for the lightbox gallery*/
 
   var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+
+
+  /* Video pop up */
+
+
+  $( document ).ready(function() {
+    $('#headerVideoLink').magnificPopup({
+     type:'inline',
+     midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+   });
+     
+   });
